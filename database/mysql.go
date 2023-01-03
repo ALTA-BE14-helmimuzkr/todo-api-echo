119f99ad1aee9c8694b07404165741e8900fd5db
helmimuzkr <mhelmimuzakir@gmail.com>
2023-01-03 22:33:00 +0700

    database configuration added

---
+++ database/mysql.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ALTA-BE14-helmimuzkr/config"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func OpenConnectionMysql(c *config.AppConfig) *gorm.DB {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", c.DBUSER, c.DBPASS, c.DBHOST, c.DBPORT, c.DBNAME)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Println("Koneksi database mysql gagal", err)
+		return nil
+	}
+
+	return db
+}
