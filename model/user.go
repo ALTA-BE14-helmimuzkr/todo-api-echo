fda606454ca9df0737c1f86cd54f0893176f5694
helmimuzkr <mhelmimuzakir@gmail.com>
2023-01-03 23:06:16 +0700

    user model added

---
+++ model/user.go
@@ -0,0 +1,12 @@
+package model
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	Email     string `json:email`
+	Password  string `json:password`
+	Telephone string `json:telephone`
+}
