343c2f85e04199436f02ade2003e099f97826a1c
helmimuzkr <mhelmimuzakir@gmail.com>
2023-01-03 23:17:33 +0700

    update package and user model

--- model/user.go
+++ model/user.go
@@ -1,6 +1,10 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"log"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
@@ -9,4 +13,19 @@ type User struct {
 	Email     string `json:email`
 	Password  string `json:password`
 	Telephone string `json:telephone`
+}
+
+type UserModel struct {
+	db *gorm.DB
+}
+
+func (model *UserModel) Insert(user User) (User, error) {
+
+	err := model.db.Create(&user).Error
+	if err != nil {
+		log.Println("CREATE USER ERROR", err)
+		return User{}, err
+	}
+
+	return user, nil
 }
