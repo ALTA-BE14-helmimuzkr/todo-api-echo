cf2e7ceae3df9af75184962a77c34bc602a6e36f
helmimuzkr <mhelmimuzakir@gmail.com>
2023-01-03 23:07:12 +0700

    main updated. setup model migration

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/ALTA-BE14-helmimuzkr/config"
 	"github.com/ALTA-BE14-helmimuzkr/database"
+	"github.com/ALTA-BE14-helmimuzkr/model"
 )
 
 func main() {
@@ -11,5 +12,8 @@ func main() {
 
 	// Setup Dataabse
 	db := database.OpenConnectionMysql(c)
+
+	// Setup Migration
+	model.Migrate(db)
 
 }
